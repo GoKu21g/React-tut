9310ecc69c3579a014236ad749e2b582cc9b17b1
GoKu21g <gouravkumar21g@gmail.com>
2026-06-28 23:56:51 +0530

    vite1

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './goku'
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Chai is not my cup of tea</h1>
+      <p>test para</p>
+    </>
+  )
+}
+
+export default App
